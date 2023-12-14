92947fd29c87eb9600a4f91ac5e07f69607c82f7
kalzeh <maksbroja@gmail.com>
2023-12-14 15:58:50 +0100

    dodalem metode solve()

--- Projekt/sudoku_functions.cpp
+++ Projekt/sudoku_functions.cpp
@@ -2,8 +2,17 @@
 #include "sudoku_functions.h"
 
 Sudoku::Sudoku(){
+    Sudoku::copyGrid();
     std::cout<<"Stworzylem gre xd "<<std::endl;
 };
+
+void Sudoku::copyGrid() {
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            plansza[i][j] = planszaB[i][j];
+        }
+    }
+}
 
 void Sudoku::Wyswietl(){
     std::cout<<std::endl<<"--------+-------+--------"<<std::endl;
@@ -11,13 +20,69 @@ void Sudoku::Wyswietl(){
         std::cout<<"| "; 
         for(int j=0; j<9; j++){
             std::cout<<plansza[i][j]<<" ";
-            if(j%3==2){
-               std::cout<<"| "; 
-            }
+            if(j%3==2)std::cout<<"| "; 
         }
         std::cout<<std::endl;
-        if(i%3==2){
-               std::cout<<"--------+-------+--------"<<std::endl; 
-            }
+        if(i%3==2) std::cout<<"--------+-------+--------"<<std::endl; 
     }
+};
+
+bool Sudoku::check(int x, int y, int a){
+    //sprawdzenie rzedu
+    for(int i = 0 ;i<9;i++){
+        if( i != y && plansza[x][i]==a) return false;
+    }
+    //sprawdzenie kolumny
+    for(int i = 0 ;i<9;i++){
+        if( i != x && plansza[i][y]==a) return false;
+    }
+    //sprawdzenie kwadratu 3x3
+    int startX = x - x%3;
+    int startY = y - y%3;
+    for(int i = 0;i<3;i++){
+        for(int j = 0; j< 3;j++){
+            if(x!=(startX+i) && y!=(startY+j) && plansza[startX+i][startY+j]==a) return false;
+        }
+    }
+    return true;
+}
+
+bool Sudoku::checkBoard(){
+    for(int i =0;i<9;i++){
+        for(int j =0;j<9;j++){
+            if(plansza[i][j]!=0){
+                if (!check(i,j,plansza[i][j])){
+                    std::cout<<"Niepoprawne jest: "<<plansza[i][j]<<" w rzedzie: "<<(i+1)<<", kolumnie: "<<(j+1)<<std::endl;
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+};
+
+bool Sudoku::findEmpty(int x, int y){
+    if(plansza[x][y]==0) return true;
+    return false;
+};
+
+bool Sudoku::Solve(){
+    if(!checkBoard()) return false;
+    for(int i =0;i<9;i++){
+        for(int j=0;j<9;j++){
+            if(findEmpty(i,j)){
+                for(int k = 1;k <= 9;k++){
+                    plansza[i][j]=k;
+                    plansza[i][j]=0;
+                    if(check(i,j,k)){
+                        plansza[i][j]=k;
+                        if(Solve())return true;
+                        plansza[i][j]=0;
+                    };
+                }
+                return false;
+            }
+        }
+    }
+    return true;
 };
--- Projekt/sudoku_functions.h
+++ Projekt/sudoku_functions.h
@@ -4,13 +4,14 @@
 
 #pragma once
 #include <iostream>
+#include <algorithm>
 
 /** 
  * Klasa Sudoku
 */
 class Sudoku{
     private:
-        int plansza[9][9] ={{0, 7, 0, 8, 9, 0, 0, 0, 0},
+        /*int planszaB[9][9] ={{0, 7, 0, 8, 9, 0, 0, 0, 0},
                             {0, 5, 0, 0, 0, 0, 3, 0, 4},
                             {0, 2, 0, 0, 4, 0, 0, 1, 0},
                             {5, 6, 8, 9, 0, 0, 4, 7, 2},
@@ -18,16 +19,64 @@ class Sudoku{
                             {0, 0, 0, 0, 5, 0, 6, 3, 8},
                             {7, 3, 0, 1, 0, 2, 0, 8, 0},
                             {6, 0, 0, 4, 7, 0, 1, 0, 0},
-                            {2, 0, 9, 0, 3, 8, 7, 0, 6}};
+                            {2, 0, 9, 0, 3, 8, 7, 0, 6}};*/
+        int planszaB[9][9] ={{9,8,7,6,5,4,3,2,1},
+                             {6,5,4,9,8,7,0,0,0},
+                             {0,0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0,0},
+                             {0,0,0,0,0,0,0,0,0},};
+        //plansza poczatkowa
+        /*int planszaB[9][9] ={{0,0,9,0,4,0,8,0,0},
+                            {0,6,0,5,0,8,0,0,2},
+                            {5,0,0,0,3,0,0,0,0},
+                            {0,4,0,0,0,0,0,7,0},
+                            {7,0,0,2,0,5,9,0,0},
+                            {0,0,0,3,0,0,0,0,0},
+                            {0,0,1,0,0,0,0,0,6},
+                            {4,0,0,7,0,9,2,0,0},
+                            {0,0,0,0,8,0,0,0,0}};*/
+        //plansza do zmiany
+        int plansza[9][9];
     public:
         /** Konstruktor
         *   @return 
         */
         Sudoku();
+        /** metoda copyGrid przekopiowuje planszeB (poczatkowa) do planszy (na niej beda wykonywane wszelkie dzialania)
+        */
+        void copyGrid();
         /** metoda Wyswietl sluzy do wypisania planszy 
-        *   
         */
         void Wyswietl();
+        /** Metoda check, sprawdza czy dana liczba jest poprawna
+        *   @param x wiersz
+        *   @param y kolumna
+        *   @param a liczba do sprawdenia
+        *   @return zwraca true jezeli liczba moze byc, false jesli nie
+        */
+        bool check(int x, int y, int a);
+        /** Metoda checkBeginnig, sprawdza czy plansza jest poprawna
+        *   @return zwraca true jezeli plansza jest poprawna, false jesli nie
+        */
+        bool checkBoard();
+
+        /** Metoda solve, rozwiazujaca zadana plansze sudoku
+        *   @param _plansza przyjmuje planszę do sudoku
+        *   @return zwraca rozwiązaną planszę
+        */
+        bool Solve();
+        /** Metoda findEmpty szuka pierwszego pustego pola (o wartosci 0)
+        *   @param x wiersz
+        *   @param y kolumna
+        *   @return zwraca true jezeli dana komorka jest pusta (o wartosci 0)
+        */
+        bool findEmpty(int x, int y);
+        
+        
 };
 
 #endif
